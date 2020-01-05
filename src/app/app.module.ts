6701167d6a394be7be42eb3d6c2837f907bc4b98
itsswill <willn2doit@gmail.com>
2020-01-05 12:31:41 -0500

    imported necessary components into module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { HttpClientModule } from '@angular/common/http';
+import { StreamsComponent } from './components/streams/streams.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    StreamsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
